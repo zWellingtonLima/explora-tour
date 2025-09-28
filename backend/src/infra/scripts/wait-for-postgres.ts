e07e588175738db34a1579e735c2035d5de5e79c
zWellingtonLima <0wellingtonlima1@gmail.com>
2025-09-28 13:31:18 +0100

    feat: add `services:wait-database` script and `wait-for-postgres`

---
+++ backend/src/infra/scripts/wait-for-postgres.ts
@@ -0,0 +1,18 @@
+import { exec } from "node:child_process";
+
+function checkPostgres() {
+  exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
+
+  function handleReturn(error: any, stdout: string) {
+    if (stdout.search("accepting connections") === -1) {
+      process.stdout.write(".");
+      checkPostgres();
+      return;
+    }
+
+    console.log("\n🟢 Postgres está pronto e aceitando conexões.");
+  }
+}
+
+process.stdout.write("\n\n🔴 Aguardando Postgres aceitar conexões");
+checkPostgres();
