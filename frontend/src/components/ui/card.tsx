4a8c27968ca218d137486cb061a8570757f2ddce
wWellingtonLima <0wellingtonlima2@gmail.com>
2025-10-31 16:21:10 +0000

    style: remove opacity

--- frontend/src/components/ui/card.tsx
+++ frontend/src/components/ui/card.tsx
@@ -7,7 +7,7 @@ function Card({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="card"
       className={cn(
-        "flex min-w-52 flex-col gap-6 rounded-xl border bg-card/90 py-6 text-card-foreground shadow-lg",
+        "flex min-w-52 flex-col gap-6 rounded-xl border bg-card py-6 text-card-foreground shadow-lg",
         className,
       )}
       {...props}
