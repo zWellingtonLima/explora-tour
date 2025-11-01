9867778e9cef3349079fa98e2ec1ec04a247102c
zWellingtonLima <0wellingtonlima1@gmail.com>
2025-11-01 17:06:49 +0000

    feat: create migrations controller and migrations model

---
+++ backend/src/models/getMigrations.ts
@@ -0,0 +1,31 @@
+import { resolve } from "node:path";
+import { runner as migrationRunner } from "node-pg-migrate";
+import { Client } from "pg";
+
+import database from "infra/database";
+
+const getMigrations = async (dryRun: boolean) => {
+  let dbClient: Client | null = null;
+
+  try {
+    dbClient = await database.getNewClient();
+
+    const pendingMigrations = await migrationRunner({
+      dbClient: dbClient,
+      dryRun, //true
+      verbose: true,
+      dir: resolve("src", "infra", "migrations"),
+      migrationsTable: "pgmigrations",
+      direction: "up",
+    });
+
+    return pendingMigrations;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  } finally {
+    await dbClient?.end();
+  }
+};
+
+export = getMigrations;
