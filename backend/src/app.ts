ca2a11ffb61ee03b8e9981ed3779664ff8c5d006
zWellingtonLima <0wellingtonlima1@gmail.com>
2025-11-21 12:08:21 +0000

    feat: add helmet, morgan and express.json middlewares to app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,18 @@
 import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
 
 import statusRouter from "./routes/status.router.ts";
 import migrationsRouter from "routes/migrations.router.ts";
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+// Security middleware by setting HTTP Headers
+app.use(helmet());
+app.use(morgan("dev")); // Log the requests
 
 app.use("/api/v1/status", statusRouter);
 app.use("/api/v1/migrations", migrationsRouter);
