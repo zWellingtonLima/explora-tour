862c0db75434f91af77bf48e495b1fc56168fe4d
zWellingtonLima <0wellingtonlima1@gmail.com>
2025-12-11 21:21:48 +0000

    feat: create `HeaderMenu` and initiate creating user sign up and login area

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,23 +1,52 @@
+import { HeaderMenu } from "./HeaderMenu";
 import { Logo } from "./Logo";
+import { Button } from "./ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import signUp from "@/assets/signup.jpg";
+import { FieldGroup, FieldSet } from "./ui/field";
 
 export function Header() {
   return (
     <header className="flex items-center justify-between gap-2 border-b-2 border-blue-100 py-4">
-      <Logo />
-      <nav>
-        <ul className="flex gap-4 text-gray-600">
-          <li>
-            <a href="/adventurer">Aventureiro</a>
-          </li>
-          <li>
-            <a href="/adventurer">Condutor</a>
-          </li>
-          <li>
-            <a href="/adventurer">Sobre nós</a>
-          </li>
-        </ul>
-      </nav>
-      <div>Login Area</div>
+      <div className="flex items-center">
+        <Logo />
+        <HeaderMenu />
+      </div>
+      <div className="flex gap-4">
+        <Dialog modal>
+          <DialogTrigger asChild>
+            <Button>Cadastrar-se</Button>
+          </DialogTrigger>
+          <DialogContent>
+            <section className="columns-2 gap-4">
+              <div>
+                <img
+                  src={signUp}
+                  alt="Grupo de aventureiros admirando paisagem"
+                  className="h-[650px] rounded-lg object-cover object-left"
+                />
+              </div>
+              <DialogHeader>
+                <DialogTitle className="text-center">Cadastrar-se</DialogTitle>
+
+                <form>
+                  <FieldGroup>
+                    <FieldSet></FieldSet>
+                  </FieldGroup>
+                </form>
+              </DialogHeader>
+            </section>
+          </DialogContent>
+        </Dialog>
+        <Button variant="outline">Entrar</Button>
+      </div>
     </header>
   );
 }
---
+++ frontend/src/components/HeaderMenu.tsx
@@ -0,0 +1,17 @@
+export function HeaderMenu() {
+  return (
+    <nav className="mt-2 ml-8">
+      <ul className="flex gap-4 text-gray-600">
+        <li>
+          <a href="/adventurer">Aventureiro</a>
+        </li>
+        <li>
+          <a href="/driver">Condutor</a>
+        </li>
+        <li>
+          <a href="/about">Sobre nós</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
