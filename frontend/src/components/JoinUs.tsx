36e83614063dc09ed74f4714ca2f5b3adbbde307
zWellingtonLima <0wellingtonlima1@gmail.com>
2025-12-11 21:26:07 +0000

    Revert "style: remove unnecessary `font-bold` style from button component"
    This reverts commit 4aa9ca0176c8093b238810f4f70eef6ff6496715.

--- frontend/src/components/JoinUs.tsx
+++ frontend/src/components/JoinUs.tsx
@@ -14,7 +14,11 @@ export function JoinUs() {
               Explorar novos lugares nunca foi tão simples.
             </h3>
             <p>Junte-se a nós e aventure-se.</p>
-            <Button variant={"outline"} size="lg" className="mt-4 text-primary">
+            <Button
+              variant={"outline"}
+              size="lg"
+              className="mt-4 font-bold text-primary"
+            >
               Buscar aventura
             </Button>
           </div>
