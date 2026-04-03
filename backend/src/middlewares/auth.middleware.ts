1c908e124a808a469b9e7060e704283e6d8e91d7
zWellingtonLima <0wellingtonlima1@gmail.com>
2026-04-03 19:15:05 +0100

    refactor: rename authMiddleware to `auth.middleware` and improve code

---
+++ backend/src/middlewares/auth.middleware.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from "express";
+
+import { jwtService } from "services/jwt.service.ts";
+import { AuthError } from "errors/Errors.ts";
+
+function authenticateToken(req: Request, _res: Response, next: NextFunction) {
+  try {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader?.split(" ")[1]; // "Bearer <token>"
+
+    if (!token) return next(new AuthError());
+
+    req.user = jwtService.verifyAccessToken(token); // lança TokenError se inválido
+    next();
+  } catch (err) {
+    next(err); // TokenError (403) → errorHandler
+  }
+}
+
+export { authenticateToken };
